0e724d22e100a39d2ed07013588fe26b6c63d812
praburangki <praburangki@gmail.com>
2016-07-21 08:33:55 +0800

    Add dashboard books

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
+
   def stuk_to_do_home
     redirect_to stuk_todo_tasks_path if current_user
   end
 
   def estuk_home
     redirect_to stuk_books_path if current_user
+  end
+
+  def estuk_dashboard
+    @stuk_books = current_user.stuk_books
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :stuk_books
   root 'pages#stuk_to_do_home'
 
+  resources :stuk_books
   resources :stuk_todo_tasks do
     member do
       put :change
     end
   end
+
+  get 'estuk_dashboard' => 'pages#estuk_dashboard'
 
   devise_for :users
   get 'pages/stuk_to_do_home', path: 'stuk_to_do', as: 'stuk_to_do'
