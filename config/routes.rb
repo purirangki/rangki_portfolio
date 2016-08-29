700dcfbfdfba1e7060302722bc71de7632d5ac9e
praburangki <praburangki@gmail.com>
2016-08-29 09:07:28 +0800

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-
   root 'pages#stuk_to_do_home'
-
-  resources :stuk_books
-
-  post '/buy/:slug', to: 'estuk_transactions#create', as: :buy
-  get '/pickup/:guid', to: 'estuk_transactions#pickup', as: :pickup
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  get 'pages/stuk_to_do_home', path: 'stuk_to_do', as: 'stuk_to_do'
+  get 'pages/estuk_home', path: 'estuk', as: 'estuk'
+  get 'pages/stuk_starter_home', path: 'stuk_starter', as: 'stuk_starter'
+  get 'pages/stuk_places_home', path: 'stuk_places', as: 'stuk_places'
 
   resources :stuk_todo_tasks do
     member do
@@ -13,13 +12,12 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :stuk_books
+
   get 'estuk_dashboard' => 'pages#estuk_dashboard'
-
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get 'pages/stuk_to_do_home', path: 'stuk_to_do', as: 'stuk_to_do'
-  get 'pages/estuk_home', path: 'estuk', as: 'estuk'
-  get 'pages/stuk_starter_home', path: 'stuk_starter', as: 'stuk_starter'
-
+  post '/buy/:slug', to: 'estuk_transactions#create', as: :buy
+  get '/pickup/:guid', to: 'estuk_transactions#pickup', as: :pickup
+  
   resources :stuk_starter_projects do
     resources :stuk_starter_rewards, only: [:new, :create, :edit, :update, :destroy]
     resources :stuk_starter_pledges
